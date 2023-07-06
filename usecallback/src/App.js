4b85cba6b6a43088be97ef2888c2bb73745f8157
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-07-06 11:29:23 +0530

    useCallback hook

---
+++ usecallback/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import ProductLists from "./components/ProductLists";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Products Lists</h1>
+      <ProductLists />
+    </div>
+  );
+}
+
+export default App;
